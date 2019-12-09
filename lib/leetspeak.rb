aa40a37dc249cf0707d29cc097d33c6e79dfaa4e
Student <example@example.com>
2019-12-09 14:18:45 -0800

    add spec and test to turn s to z except at index 0.

--- lib/leetspeak.rb
+++ lib/leetspeak.rb
@@ -3,8 +3,16 @@ class String
     word = []
     letters = self.chars
     letters.each do |letter|
-      if letter === "e"
+      if (letter === "E") | (letter === "e")
         word.push("3")
+      elsif (letter === "o") | (letter === "O")
+        word.push("0")
+      elsif letter === "I"
+        word.push("1")
+      elsif ((letter === "S") | (letter === "s")) & !(word[0])
+        word.push(letter)
+      elsif (letter === "S") | (letter === "s")
+        word.push("z")
       else
         word.push(letter)
       end
